56e2c7b3c352f8976cf1f76da787191aaebb3d90
Robin Heinze <robin.m.heinze@gmail.com>
2023-05-15 14:58:49 -0700

    switch to react-player and s3

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import React from "react";
-import screenfull from "screenfull";
-import YouTube from "react-youtube";
-import DeviceDetector from "device-detector-js";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
+import ReactPlayer from "react-player";
 
-const VIDEO_ID = "2o4QgRiuvjw";
-export default function YTPlayer(props) {
-  const dd = new DeviceDetector();
-  const mobile = dd.usesMobileBrowser();
-  const fullScreen = (event) => {
-    var iframe = event.target.getIframe();
-    if (screenfull.isEnabled) {
-      screenfull.request(iframe);
-    }
-  };
-  const opts = {
-    height: "100%",
-    width: "100%",
-    autoplay: "1",
-  };
-  const useStyles = makeStyles({
-    videoDiv: {
-      width: "100%",
-      height: "100%",
-    },
-  });
-  const classes = useStyles();
+function App() {
   return (
-    <YouTube
-      style={{ height: "100vh" }}
-      containerClassName={classes.videoDiv}
-      videoId={VIDEO_ID}
-      opts={opts}
-      onPlay={() => (mobile ? fullScreen : {})}
-      replay={true}
-    />
+    <div className="App">
+      <ReactPlayer
+        url="https://s3.us-east-1.amazonaws.com/ir_public/media/RNR-Promotion-Video.mp4"
+        playing
+        loop
+        muted
+        width="100%"
+        height="100vh"
+      />
+    </div>
   );
 }
-YTPlayer.propTypes = {
-  id: PropTypes.string,
-};
+
+export default App;
